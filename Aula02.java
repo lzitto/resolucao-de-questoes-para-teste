11e54b9ef7302eda591b0e8d71c93d6cdfaeeda0
lzitto <david.leonam.holanda@ccc.ufcg.edu.br>
2024-03-16 15:49:39 -0300

    rs

--- Aula02.java
+++ Aula02.java
@@ -5,11 +5,21 @@ public class Aula02 {
 		Caneta c1 = new Caneta();
 		c1.cor = "Azul";
 		c1.ponta = 0.5f;
-		c1.tampada = false;
+		c1.tampada = true;
 		c1.modelo = "BIC";
 		c1.carga = 90;
 		c1.destampar();
 		c1.tampar();
 		c1.status();
+		c1.rabiscar();
+		
+		System.out.println();
+		
+		Caneta c2 = new Caneta();
+		c2.cor = "Preta";
+		c2.tampada = false;
+		c2.status();
+		c2.rabiscar();
+		
 	}
 }
